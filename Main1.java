cd9cee6342a6e6283efecba38ff4067d17ff535d
Heerak Tiwari <ht5630@srmist.edu.in>
2026-02-16 11:15:51 +0530

    updated main1

--- Main1.java
+++ Main1.java
@@ -1,11 +1,11 @@
 public class Main1{
     public static void main(String[] args){
-        System.out.println("   ***   "+"   ***   "+"****"+    "   ****");
-        System.out.println(" **   ** "+" **   ** "+"**  **"+  " **");
-        System.out.println("**     **"+"**     **"+"**   **"+ "**");
-        System.out.println("**     **"+"**     **"+"**  **"+  " **");
-        System.out.println("**     **"+"**     **"+"****"+    "  ***");
-        System.out.println(" **   ** "+"**     **"+"**"+      "    **");
-        System.out.println("   ***   "+"   ***   "+"**"+      "****");
+        System.out.println("   ***   " + " " + "   ***   " + " " + "****"    + "     " +   "*****");
+        System.out.println(" **   ** " + " " + " **   ** " + " " + "**  **"  + "   " +   "*");
+        System.out.println("**     **" + " " + "**     **" + " " + "**   **" + "  " +   "*");
+        System.out.println("**     **" + " " + "**     **" + " " + "**  **"  + "   " +   "*****");
+        System.out.println("**     **" + " " + "**     **" + " " + "****"    + "     " +   "    *");
+        System.out.println(" **   ** " + " " + "**     **" + " " + "**"      + "       " +   "    *");
+        System.out.println("   ***   " + " " + "   ***   " + " " + "**"      + "       " +   "*****");
     }
 }
